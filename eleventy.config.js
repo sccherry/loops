b3ee104d70704e1baada3efba7b3e673f467d8d6
Steve Cherry <stevenccherry@gmail.com>
2021-10-13 09:52:09 -0400

    Add blank eleventy site

---
+++ eleventy.config.js
@@ -0,0 +1,10 @@
+module.exports = function (eleventyConfig) {
+  return {
+    dataTemplateEngine: 'njk',
+    dir: {
+      input: 'src',
+      output: 'dist',
+      layouts: '_layouts',
+    },
+  };
+};
