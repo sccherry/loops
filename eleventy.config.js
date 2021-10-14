3d22321aad99b0c5e386d4837165200e9929d1af
Steve Cherry <stevenccherry@gmail.com>
2021-10-14 19:41:23 +0000

    Add navigation

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,12 @@
+const eleventyNavigationPlugin = require('@11ty/eleventy-navigation');
+
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
   eleventyConfig.addPassthroughCopy('public');
 
   return {
+    htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     dir: {
       input: 'src',
