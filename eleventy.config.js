eae7e5c6558373026a8354dc379aae24e8775283
Steve Cherry <stevenccherry@gmail.com>
2022-01-31 16:26:08 -0500

    Change file extension to njk

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('public');
 
   return {
-    htmlTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
     dir: {
       input: 'src',
